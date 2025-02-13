bff868348ebf7de2fc90ed76f18dc6bfe10ac985
Kevin Campos Venegas <kenderman.8@gmail.com>
2025-02-13 11:57:09 -0300

    update js functions

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ console.log('Add task button clicked');
 const searchBar = document.getElementById('search-bar');
 const btnYouTube = document.getElementById('youtube-search');
 const btnTikTok = document.getElementById('tiktok-search');
-const btnGoogle = document.getElementById('google-search');
-const btnXCom = document.getElementById('xcom-search');
-const btnReddit = document.getElementById('reddit-search');
-// const btnChatGPT = document.getElementById('chatgpt-search');
 const btnGoogleMaps = document.getElementById('google-maps-search');
 
 // Función para abrir la búsqueda en una plataforma específica
@@ -22,18 +18,7 @@ function performSearch(baseURL) {
 // Eventos click en los botones
 btnYouTube.addEventListener('click', () => performSearch("https://www.youtube.com/results?search_query="));
 btnTikTok.addEventListener('click', () => performSearch("https://www.tiktok.com/search?q="));
-btnGoogle.addEventListener('click', () => performSearch("https://www.google.com/search?q="));
-btnXCom.addEventListener('click', () => performSearch("https://x.com/search?q="));
-btnReddit.addEventListener('click', () => performSearch("https://www.reddit.com/r/all/search?q="));
-btnGoogleMaps.addEventListener('click', () => {
-    const query = searchBar.value.trim();
-    if (query) {
-        // Abre Google Maps con la consulta
-        window.open(`https://www.google.com/maps/search/${encodeURIComponent(query)}`, '_blank');
-    } else {
-        alert('Por favor, ingresa una ubicación o dirección para Google Maps.');
-    }
-});
+btnGoogleMaps.addEventListener('click', () => performSearch("https://www.google.com/maps/search/"));
 
 // Evento keydown en el searchBar
 searchBar.addEventListener('keydown', (event) => {
